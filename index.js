8856f233132f6b8acf7732589a7e3d94f2560549
timoTHE-OS <popoolagospel@students.unilorin.edu.ng>
2024-02-26 12:31:49 +0100

    Class notes

---
+++ index.js
@@ -0,0 +1,24 @@
+ var name = 'Jamiu' ;
+//  console.log(str.charAt(0));
+
+
+const myName = "Jamiu";
+const str1 = "Hello";
+const str2 = "World";
+
+
+
+const str = 'Hello World';
+console.log(str.includes('World'));
+console.log(myName.charAt(3));
+console.log(str.indexOf('o'));
+// console.log(myName.);
+console.log(str.lastIndexOf('o'));
+
+const greetings = 'Welcome Tunde';
+console.log(greetings.slice(8, 13));
+console.log(greetings.split(,));
+//substring
+//concat
+//type of
+
